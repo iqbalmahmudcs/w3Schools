7a437905f38f2ed074937e4cae2883fdddbf7a86
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-04-17 19:25:38 +0600

    zzz

--- src/05Constants/obj/Debug/net9.0/05Constants.AssemblyInfo.cs
+++ src/05Constants/obj/Debug/net9.0/05Constants.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("05Constants")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b967242096616777cb3cd15279f6a09498ac750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbae32d3344da45d192cae7983842a0a92c4fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("05Constants")]
 [assembly: System.Reflection.AssemblyTitleAttribute("05Constants")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
