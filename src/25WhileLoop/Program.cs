cd5109825c2af2c2e39f257eec4c556d7df44cb4
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-04-24 13:05:14 +0600

    While & Do/While Loops are covered

---
+++ src/25WhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿//The while loop loops through a block of code as long as specified condition is true
+
+int i = 1;
+while (i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+Console.WriteLine();
+
+//The Do/While Loop
+//The Ddo/while loop is a variant of the while loop. THus loop will execute the code block onece before checking the conditions
+i = 1;
+do
+{
+    Console.WriteLine(i);
+    i++;
+}
+while (i < 1);
+
+
+//Note: Don't forgot  to increase the variable used in the condition, otherwose the loop will never end!
