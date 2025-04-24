8de944103407f9f6417afcbe37c7acbe37766738
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-04-24 18:42:40 +0600

    Loops, Break and continue statements

---
+++ src/26ForLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿// When you already knows that how many times you want to loop through a block of code, use the for loop instead of a while loop.
+
+using System.Runtime.CompilerServices;
+
+for (int i = 0; i< 10; i++)
+    Console.WriteLine(i);
+
+//Nested Loops
+//It is also possible to place a loop inside another loop. This is called nested loop
+//The inner Loop is executed for each cycle of outer loop
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("This is outer loop");
+    for (int j = 0; j < 5; j++)
+    {
+        Console.WriteLine(j);
+    }
+}
+
+
+
+//Foreach Loop
+//There is also a foreach loop which is used exclusively to loop through elements in an array or an other data set
+
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+foreach(string i in cars)
+    Console.WriteLine(i);
+
+
+
+
+
+
+
+
---
+++ src/27BreakANDContinue/Program.cs
@@ -0,0 +1,47 @@
+﻿//The break statement is used for jump out of a loop
+for(int i = 0; i< 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 5)
+        break;
+}
+
+
+//The continue statement breaks one cycle of loop, if specified condition occours, and continues with the next cycle of loop.
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine("pre" + i);
+    if (i == 5)
+        continue;
+    Console.WriteLine(i);
+}
+
+
+//break in while loop
+int i = 0;
+while (i < 10)
+{
+    Console.WriteLine(i);
+    i++;
+    if (i == 4)
+    {
+        break;
+    }
+}
+
+
+//continue in while loop
+i = 0;
+while (i < 10)
+{
+    if (i == 4)
+    {
+        i++;
+        continue;
+    }
+    Console.WriteLine(i);
+    i++;
+}
+
+
+
