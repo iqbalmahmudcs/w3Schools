e59071ca8ad9d028388502c5d96b5c5d523ef6ec
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-05-26 18:38:06 +0600

    OOP

---
+++ src/37OOP/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _37OOP
+{
+    public class Car
+    {
+        //Fields
+        public string model;
+        public string color;
+        public int year;
+
+
+
+        //Methods
+        public void Display()
+        {
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Year: {year}");
+        }
+
+    }
+}
---
+++ src/37OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using _37OOP;
+
+/*
+ * OOP is faster and easier to execute.
+ * Provides clear structure for the programs.
+ * Helps to keep the code DRY(Dont Repeat Yourself)
+ */
+
+
+Car ford = new Car();
+ford.model = "Mustang";
+ford.color = "red";
+ford.year = 1969;
+
+Car opel = new Car();
+opel.model = "Astra";
+opel.color = "white";
+opel.year = 2005;
+
+ford.Display();
+opel.Display();
+
+
+
+
+
+
+
+
+
+
+
+
